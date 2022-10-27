e8bfe2822c770801ef1b577fd0e4891c15873b02
Subham Das <89313464+SubhamDas1997@users.noreply.github.com>
2022-10-27 13:53:24 +0530

    removed warnings.

--- src/main/java/studentRegistration/security/WebSecurityConfig.java
+++ src/main/java/studentRegistration/security/WebSecurityConfig.java
@@ -6,9 +6,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import studentRegistration.serviceImpl.UserServiceImpl;
